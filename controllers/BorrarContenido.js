1d232a53f17930175b501d0f441b2c6337cb5e20
AaronRdz69 <aaron.rodriguez@metricamovil.com>
2024-02-02 08:54:23 -0600

    nothing here

--- controllers/BorrarContenido.js
+++ controllers/BorrarContenido.js
@@ -2,13 +2,9 @@ const Contenido = require('../models/Contenido');
 
 module.exports = async (req,res)=>{
     try {
-        const contenidoId = req.query.id; // Asumiendo que el ID se pasa como parámetro en la URL
-
-        // Eliminar el contenido específico
-        await Contenido.findByIdAndDelete(contenidoId);
-
-        // Redireccionar a la página donde se listan los contenidos o mostrar un mensaje de éxito
-        res.redirect('/PanelContenido'); // Actualiza con la ruta correcta
+        const { id } = req.query; // Obtiene el id del contenido desde los parámetros de la consulta
+        await Contenido.findByIdAndDelete(id); // Elimina el contenido por ID
+        res.redirect('/PanelContenido'); // Redirige de vuelta al panel de contenido
     } catch (error) {
         console.error('Error al eliminar el contenido:', error);
         res.status(500).send('Error al eliminar el contenido');
--- controllers/EditarContenido.js
+++ controllers/EditarContenido.js
@@ -1,17 +1,28 @@
 const Contenido = require('../models/Contenido');
 
-module.exports = async (req,res)=>{
+module.exports = async (req, res) => {
     try {
-        const { titulo, Descripcion } = req.body;
-        const contenidoId = req.body.id;
-    
-        // Update the content in the database
-        await Contenido.findByIdAndUpdate(contenidoId, { titulo, Descripcion });
-    
-        // Redirect to the updated content page or any other desired page
-        res.redirect(`/PanelContenido`);
-      } catch (error) {
-        console.error(error);
-        res.status(500).send('Error updating content');
-      }
-}
+        const { titulo, Descripcion, id } = req.body; // Asegúrate de que los nombres coincidan con los de tu formulario
+
+        // Comprueba si los arrays tienen la misma longitud para evitar errores
+        if (titulo.length === Descripcion.length && titulo.length === id.length) {
+            // Itera sobre los arrays y actualiza cada elemento de contenido
+            for (let i = 0; i < id.length; i++) {
+                await Contenido.findByIdAndUpdate(id[i], {
+                    titulo: titulo[i],
+                    Descripcion: Descripcion[i]
+                });
+            }
+
+            // Redirige a la página del panel de contenido con un mensaje de éxito
+            res.redirect('/PanelContenido');
+        } else {
+            // Lanza un error si las longitudes de los arrays no coinciden
+            throw new Error('La longitud de los arrays no coincide');
+        }
+    } catch (error) {
+        console.error('Error actualizando el contenido:', error);
+        // Envía una respuesta de error al cliente
+        res.status(500).send('Error al actualizar el contenido');
+    }
+};
